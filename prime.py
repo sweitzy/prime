124d1ed554d7af0ead1216c5add9c181fdc747a4
Scott Weitzenkamp <scott_weitzenkamp@yahoo.com>
2026-01-03 22:42:22 -0600

    remove comments and add command-line args

--- prime.py
+++ prime.py
@@ -2,59 +2,56 @@
 
 # prime.py: calculate prime numbers
 
-# $Id: prime.py,v 1.5 2025/08/26 16:06:28 scott Exp scott $
+# $Id: prime.py,v 1.6 2026/01/04 04:42:22 scott Exp scott $
 
+import sys
 import math
 
 def prime(min, max):
     num_primes = 0 
 
-    # check for min > max
     if min > max:
-        # TODO: stderr
-        print(f"ERROR: min {min} must be <= max {max}")
-        return -1
+        return num_primes
 
-    # if all negative numbers, nothing to do
     if max < 2:
         return num_primes
     
-    # special case < 3: 2 is prime
     if min < 3:
-        ###print("2")
         num_primes += 1
         
     for i in range(3, max + 1):
-        ###print(f"CHECKING {i}")
-
-        # special case: even number
         if i % 2 == 0:
-            ###print(f"  {i} EVEN NOT PRIME")
             continue
 
         is_prime = True
 
         for up in range(3, int(math.sqrt(i)) + 1, +2):
-            ###print(f"i {i} up {up}")
             if i % up == 0:
-                ###print(f"i {i} mod up {up} is not prime")
                 is_prime = False
                 break
 
         if is_prime:
-            ###print(f"{i}")
             num_primes += 1
-        #else:
-            ###print(f"  {i}: NOT PRIME")
 
     return num_primes
 
-###print("Entering main.")
+def main():
+    min_val = 1
+    max_val = 50000000
 
-# TODO: command-line arguments
+    argc = len(sys.argv)
+    if argc == 3:
+        min_val = int(sys.argv[1])
+        max_val = int(sys.argv[2])
+    elif argc == 2:
+        max_val = int(sys.argv[1])
 
-min = 1
-max = 50000000
-num_primes = prime(min, max)
-#num_primes = prime(1, 100)
-print(f"min = {min}, max = {max}, num_primes = {num_primes}")
+    if min_val > max_val:
+        print(f"ERROR: min {min_val} must be <= max {max_val}", file=sys.stderr)
+        sys.exit(1)
+
+    num_primes = prime(min_val, max_val)
+    print(f"min = {min_val}, max = {max_val}, num_primes = {num_primes}")
+
+if __name__ == "__main__":
+    main()
