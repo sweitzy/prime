7359168773c0d977ee55ea786ae1ebfcc4b0f5f6
Scott Weitzenkamp <scott_weitzenkamp@yahoo.com>
2026-01-04 00:18:22 -0600

    sqrt call optimization

--- prime.convert.pl
+++ prime.convert.pl
@@ -1,27 +1,27 @@
 #!/usr/bin/env perl
 
-# $Id: prime.convert.pl,v 1.1 2026/01/04 06:06:15 scott Exp scott $
+# $Id: prime.convert.pl,v 1.2 2026/01/04 06:18:22 scott Exp scott $
 
 use strict;
 use warnings;
+use POSIX qw(floor);
+use Scalar::Util qw(looks_like_number);
 
 sub prime {
-    my ($min, $max) = @_;
+    my ($min_val, $max_val) = @_;
     my $num_primes = 0;
 
-    # if all negative numbers, nothing to do
-    return $num_primes if $max < 2;
+    return $num_primes if $max_val < 2;
 
-    # special case < 3: 2 is prime
-    $num_primes++ if $min < 3;
+    $num_primes++ if $min_val < 3;
 
-    for my $i (3 .. $max) {
-        # special case: even number
+    for my $i (3 .. $max_val) {
         next if $i % 2 == 0;
 
         my $is_prime = 1;
+        my $limit = floor(sqrt($i));
 
-        for (my $up = 3; $up * $up <= $i; $up += 2) {
+        for (my $up = 3; $up <= $limit; $up += 2) {
             if ($i % $up == 0) {
                 $is_prime = 0;
                 last;
@@ -34,21 +34,27 @@ sub prime {
     return $num_primes;
 }
 
-# main
-my ($min, $max) = (-2, 50000000);
+sub main {
+    my @argv = @_;
+    my $min_val = 1;
+    my $max_val = 50000000;
 
-if (@ARGV == 2) {
-    $min = $ARGV[0];
-    $max = $ARGV[1];
-} elsif (@ARGV == 1) {
-    $max = $ARGV[0];
+    if (@argv == 2) {
+        $min_val = $argv[0];
+        $max_val = $argv[1];
+    } elsif (@argv == 1) {
+        $max_val = $argv[0];
+    }
+
+    if ($min_val > $max_val) {
+        print STDERR "ERROR: min $min_val must be <= max $max_val\n";
+        return 1;
+    }
+
+    my $num_primes = prime($min_val, $max_val);
+
+    print "min = $min_val, max = $max_val, num_primes = $num_primes\n";
+    return 0;
 }
 
-if ($min > $max) {
-    die "ERROR: min $min must be <= max $max\n";
-}
-
-my $num_primes = prime($min, $max);
-
-print "min = $min, max = $max, num_primes = $num_primes\n";
-exit 0;
+exit main(@ARGV);
