55b738f99ad2f86be7e30f232270f7be193133c0
Scott Weitzenkamp <scott_weitzenkamp@yahoo.com>
2026-01-03 23:41:51 -0600

    change min/max to min_val/max_val for codeconvert.ai, and call sqrt() like Python code does

--- prime.convert.c
+++ prime.convert.c
@@ -1,30 +1,33 @@
 // prime.convert.c: stripped down code to feed to http://codeconvert.ai
 
-// $Id: prime.convert.c,v 1.2 2026/01/04 05:24:54 scott Exp scott $
+// $Id: prime.convert.c,v 1.3 2026/01/04 05:41:51 scott Exp scott $
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-int prime(int min, int max) {
+int prime(int min_val, int max_val) {
   int num_primes = 0;
   
-  if (max < 2) {
+  if (max_val < 2) {
     return num_primes;
   }
 
-  if (min < 3) {
+  if (min_val < 3) {
     num_primes++;
   }
 
-  for (int i = 3; i <= max; i++) {
-
+  for (int i = 3; i <= max_val; i++) {
     if (i % 2 == 0) {
       continue;
     } 
 
     int is_prime = true;
 
-    for (int up = 3; up * up <= i; up += 2) {
+    int limit = (int)sqrt(i);
+
+    for (int up = 3; up <= limit; up += 2) {
       if (i % up == 0) {
 	is_prime = false;
 	break;
@@ -42,24 +45,23 @@ int prime(int min, int max) {
 int main(int argc, char **argv) {
   int num_primes = -1;
 
-  // defaults
-  int min = 1;
-  int max = 50000000;
+  int min_val = 1;
+  int max_val = 50000000;
   
   if (argc == 3) {
-    min = atoi(argv[1]);
-    max = atoi(argv[2]);
+    min_val = atoi(argv[1]);
+    max_val = atoi(argv[2]);
   } else if (argc == 2) {
-    max = atoi(argv[1]);
+    max_val = atoi(argv[1]);
   }
 
-  if (min > max) {
-    fprintf(stderr, "ERROR: min %d must be <= max %d\n", min, max);
+  if (min_val > max_val) {
+    fprintf(stderr, "ERROR: min %d must be <= max %d\n", min_val, max_val);
     return 1;
   }
 
-  num_primes = prime(min, max);
+  num_primes = prime(min_val, max_val);
 
-  printf("min = %d, max = %d, num_primes = %d\n", min, max, num_primes);
+  printf("min = %d, max = %d, num_primes = %d\n", min_val, max_val, num_primes);
   return 0;
 }
